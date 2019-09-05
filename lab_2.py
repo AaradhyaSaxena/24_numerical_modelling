d3376d02550849959da5768ede77e150531cffb7
AaradhyaSaxena <aaradhya271197@gmail.com>
2019-09-05 12:16:50 +0530

    2

--- lab_2.py
+++ lab_2.py
@@ -24,26 +24,35 @@ big_matrix = np.array([[0,0,1,0,0,0,0,0,0,0,0,0]
 					 ,[0,0,0,0,0,0,6,1,0,-6,-1,0]
 					 ,[1,0,0,0,0,0,0,0,0,0,0,0]])
 
-y1 = np.array([0,2,2,3,3,5,5,7,7,0,0,0])
+y1 = np.array([0,2,2,3,3,5,5,7,0,0,0,0])
 
 coeffe = np.matmul(inv(big_matrix), y1)
 
 print("Co-effe matrix : ")
 print(coeffe)
 
-##################################################
+######
 
+f = interpolate.interp1d(x, y, kind='cubic')
 
+xnew = np.arange(0,4,0.01)
+ynew = f(xnew)
 
+plt.plot(x,y, "o",xnew, ynew,"-")
+plt.show()
 
+#######
 
+from scipy.interpolate import lagrange
 
+x_l = np.array([0,1,2,3,4])
+y_l = np.array([0,2,3,5,7])
 
-# f = interpolate.interp1d(x, y, kind='cubic')
+poly = lagrange(x_l, y_l)
 
-# xnew = np.arange(0,4,0.01)
-# ynew = f(xnew)
+xnew_l = np.arange(0,4,0.01)
+ynew_l = poly(xnew_l)
 
-# plt.plot(x,y, "o",xnew, ynew,"-")
-# plt.show()
+plt.plot(x_l,y_l, "o",xnew_l, ynew_l,"-")
+plt.show()
 
